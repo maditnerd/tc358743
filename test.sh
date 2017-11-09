f617a440636e90e70fe676e9c25366e6ac45b892
Rémi Sarrailh <remi@madnerd.org>
2017-11-09 22:16:06 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 v4l2-ctl --set-edid=file=testEDID.txt --fix-edid-checksums
-./yavta --capture=1000 -n 3 --encode-to=file.h264 -f UYVY -m -T /dev/video0
+yavta/yavta --capture=1000 -n 3 --encode-to=file.h264 -f UYVY -m -T /dev/video0
